f54224518320cd75d3cab6f263b7ed6d86b24849
Mustafa Gulab Mokashi <87611344+neyaadeez@users.noreply.github.com>
2023-03-11 21:28:51 +0530

    Create Best Time to Buy and Sell.java

---
+++ Best Time to Buy and Sell.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int finval = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }
+            if((prices[i]-min) > finval){
+                finval = prices[i]-min;
+            }
+        }
+        return finval;
+    }
+}
